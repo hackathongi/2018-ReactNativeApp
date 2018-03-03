cce314b15bc3504042897d8ae1545ee49d23341c
Pere Malaret <pere.malaret@protonmail.com>
2018-03-03 14:08:02 +0100

    feat: Component Icon afegit

--- components/ItemInfo.js
+++ components/ItemInfo.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Image, Text, View } from 'react-native'
 import styled from 'styled-components'
+import Icon from './Icon'
 
-
+const BlackView = styled.View`
+    background-color:red;
+`
 
 export default class ItemInfo extends React.Component{
 
     render(){
         return(
-            <View>
-                {/**<Image />*/}
+            <BlackView >
+                <Icon/>
                 <Text>{this.props.title}</Text>
                 <Text>{this.props.subtitle}</Text>
-            </View>
+            </BlackView>
         )
     }
 }
