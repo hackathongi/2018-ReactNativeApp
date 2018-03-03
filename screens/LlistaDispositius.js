289691c3e0ce9216e3f8a33653a35d53df5863eb
David Asensio Cañas <d_asensio@medmgmt-152.tajen.edu.tw>
2018-03-03 16:44:37 +0100

    fix: Extreta logica de dades del component List.

--- screens/LlistaDispositius.js
+++ screens/LlistaDispositius.js
@@ -2,19 +2,63 @@ import React from 'react';
 import { Text, View } from 'react-native'
 import styled from 'styled-components';
 
-import List from '../components/List'
+import capitalize from 'capitalize'
+import { parse } from 'url';
 
+import List from '../components/List'
+import ListItem from '../components/ListItem'
 
 export default class LlistaDispositius extends React.Component{
     static Wrapper = styled.View`
         height: 100%;
     `
 
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            devices: []
+        }
+    }
+
+    componentWillMount () {
+        this._getDispositiusAPI()
+    }
+
     render(){
+        const { devices } = this.state
         return(
             <LlistaDispositius.Wrapper>
-                <List type='dispositius'/>
+                <List
+                    data={devices}
+                    keyExtractor={device => device.id}
+                    renderItem={(device) => (
+                        <ListItem {...device}/>
+                    )}
+                />
             </LlistaDispositius.Wrapper>
         )
+    }
+
+    async _getDispositiusAPI () {
+        try{
+            const res = await fetch(`http://84.89.60.4/v2/entities`)
+            const responseJson = await res.json()
+            
+            const devices = responseJson.map((deviceData, index) => {
+                return {
+                    id: index,
+                    title: capitalize(deviceData.id),
+                    subtitle: deviceData.description.value
+                }
+            })
+
+            this.setState({
+                devices
+            })
+        }
+        catch(error){
+            console.log('Error' . error) 
+        }
     }
 }
